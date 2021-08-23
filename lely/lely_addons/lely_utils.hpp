ef8219544565d66091c47f572985392a8e3e189f
kikass13 <nickfiege999@gmail.com>
2021-08-23 11:44:20 +0200

    added lely addons for conversion functions and stuff

---
+++ lely/lely_addons/lely_utils.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <modm/board.hpp>
+#include <lely/can/msg.h>
+
+
+static void convert(const modm::can::Message& in, can_msg& out){
+
+}
+
+static void convert(const can_msg& in, modm::can::Message& out){
+    
+}
+
